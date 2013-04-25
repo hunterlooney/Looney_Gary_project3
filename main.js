daad7d7094c9b7d558f86b1d00d6a5c6caeb8a36
Gary W. H. Looney <hunterlooney@fullsail.edu>
2013-04-25 19:29:03 -0400

    Delete Character Working!!

--- main.js
+++ main.js
@@ -18,8 +18,13 @@ var glassesValue = function () {
 		glassesValue = "No";
 	}
 };
-var saveData = function () {
-	var id = Math.floor(Math.random()*10000001);
+var saveData = function (key) {
+	if (!key){
+		var id = Math.floor(Math.random()*10000001);
+	} else { 
+		id = key;
+	}
+
 	var item = {};
 		item.name = ["Name: ", cname.value];
 		item.glasses = ["Glasses: ", glassesValue.value];
@@ -47,8 +52,10 @@ var getTheData = function () {
 	for ( var i = 0, len = localStorage.length; i < len; i++) {
 		var makeli = document.createElement('li');
 			makeTheList.appendChild(makeli);
+		
 		var key = localStorage.key(i);
 		var value = localStorage.getItem(key);
+		
 		var obj = JSON.parse(value);
 		var makeSecondList = document.createElement('ul');
 			makeli.appendChild(makeSecondList);
@@ -80,10 +87,20 @@ function makeLinksButtons (key, links) {
 	deleteTheLink.href = "#";
 	deleteTheLink.key = key;
 	var deleteTheLinkText = "Delete Character";
-	//deleteTheLink.addEventListener("click", deleteTheItem);
+	deleteTheLink.addEventListener("click", deleteTheItem);
 	deleteTheLink.innerHTML = deleteTheLinkText;
 	links.appendChild(deleteTheLink);
 	
+}
+
+function deleteTheItem () {
+	var askQuestion = confirm("Are you sure you want to delete this character?");
+	if(askQuestion) {
+		localStorage.removeItem(this.key);
+		window.location.reload();
+	} else {
+		alert("Character was NOT deleted.")
+	}
 }
 
 function editTheItem () {
@@ -97,21 +114,54 @@ function editTheItem () {
 	var radios = document.forms[0].sex;
 	for(var i = 0; i<radios.length; i++) {
 		if (radios[i].value == "Male" && item.sex[1] == "Male") {
-			radio[i].setAttribute("checked", "checked");
+			radios[i].setAttribute("checked", "checked");
 		} else if (radios[i].value == "Female" && item.sex[1] == "Female") {
-			radio[i].setAttribute("checked", "checked");
+			radios[i].setAttribute("checked", "checked");
 		}
 	}
 	if (item.glasses[1] == "Yes") {
 		('glasses').setAttribute("checked", "checked");
 	}
 	
+	save.removeEventListener("click", saveData);
 	
+	('submitButton').value = "Edit Contact";
+	var editTheSubmit = ('submit');
+	editTheSubmit.addEventListener("click", validate);
+	editTheSubmit.key = this.key;
 	
 }
 
+
+function validate (event) {
+	var getTHEcname = ('cname');
+
+	var messageArray = [];
+
+	if(getTHEcname.value === ""){
+		var cnameError = "Please enter a character name.";
+		getTHEcname.style.border = "1px solide red";
+		messageArray.push(cnameError);
+	}
+	
+	if(messageArray.length >= 1) {
+		for (var i = 0, j = messageArray.length; i< j; i++){
+			var errorText = document.createElement('li');
+			errorText.innerHTML = messageArray[i];
+			errorMessage.appendChild(errorText);
+			 
+		}
+		event.preventDefault();
+		return false;	
+	} else {
+		saveData (this.key);
+	}
+}
+
 var displayButton = document.getElementById('displayButton');
+var errorMessage = ('error');
+
 
 clearButton.addEventListener("click", clearData);
 displayButton.addEventListener("click", getTheData);
-submitButton.addEventListener("click", saveData);
+submitButton.addEventListener("click", validate);
